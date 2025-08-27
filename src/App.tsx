50380d480a9dde34b8499f934eb9b5a409daadfb
Talha Bugra Bulut <talhabugrabulut@gmail.com>
2025-08-27 11:19:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,203 @@
+import { contractPrincipalCV, cvToJSON, hexToCV, principalCV, serializeCV } from '@stacks/transactions';
+import { useState } from 'react';
+import incentives from './lp-incentives';
+
+const user = "SP3XD84X3PE79SHJAZCDW1V5E9EA8JSKRBPEKAEK7";
+
+const calls = [
+  ["SP3Y2ZSH8P7D50B0VBTSX11S7XSG24M1VB9YFQA4K.token-aeusdc", "get-name"],
+  ["SP3Y2ZSH8P7D50B0VBTSX11S7XSG24M1VB9YFQA4K.token-aeusdc", "get-symbol"],
+  ["SP3Y2ZSH8P7D50B0VBTSX11S7XSG24M1VB9YFQA4K.token-aeusdc", "get-decimals"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-name"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-symbol"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-decimals"],
+  ["SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token", "get-name"],
+  ["SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token", "get-symbol"],
+  ["SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token", "get-decimals"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-collateral", serializeCV(contractPrincipalCV("SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4", "sbtc-token"))],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-lp-params"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-debt-params"],
+  ["SP3Y6GFKWN50HPA8RKRXMY0EXAJR9VXPY899P88JN.state-v1", "get-accrue-interest-params"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "free-liquidity"],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-reserve-balance"],
+  ["SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token", "get-balance", serializeCV(contractPrincipalCV("SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA", "state-v1"))],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-user-collateral", serializeCV(principalCV(user)), serializeCV(contractPrincipalCV("SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4", "sbtc-token"))],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1", "get-user-position", serializeCV(principalCV(user))],
+  ["SP3Y2ZSH8P7D50B0VBTSX11S7XSG24M1VB9YFQA4K.token-aeusdc", "get-balance", serializeCV(principalCV(user))],
+  ["SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token", "get-balance", serializeCV(principalCV(user))],
+  ["SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.linear-kinked-ir-v1", "get-ir-params"],
+  ["SP3BJR4P3W2Y9G22HA595Z59VHBC9EQYRFWSKG743.withdrawal-caps-v1", "get-lp-cap-factor"],
+  ["SP3BJR4P3W2Y9G22HA595Z59VHBC9EQYRFWSKG743.withdrawal-caps-v1", "get-debt-cap-factor"],
+  ["SP3BJR4P3W2Y9G22HA595Z59VHBC9EQYRFWSKG743.withdrawal-caps-v1", "get-collateral-cap-factor", serializeCV(contractPrincipalCV("SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4", "sbtc-token"))],
+  ["SP3BJR4P3W2Y9G22HA595Z59VHBC9EQYRFWSKG743.withdrawal-caps-v1", "get-lp-bucket"],
+  ["SP3BJR4P3W2Y9G22HA595Z59VHBC9EQYRFWSKG743.withdrawal-caps-v1", "get-debt-bucket"],
+  ["SP3BJR4P3W2Y9G22HA595Z59VHBC9EQYRFWSKG743.withdrawal-caps-v1", "get-collateral-bucket", serializeCV(contractPrincipalCV("SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4", "sbtc-token"))],
+  ...incentives.map(i => [`${i.contract.principal}.${i.contract.id}`, "get-user-rewards", serializeCV(principalCV(user))])
+];
+
+
+function App() {
+  const [resp, setResp] = useState({});
+  const [took, setTook] = useState(0);
+  const [loading, setLoading] = useState(false)
+
+  const fetchClicked = async () => {
+    setTook(0);
+    setLoading(true);
+
+    const t1 = Date.now();
+
+    let resp;
+
+    try {
+      resp = await fetch('https://multi-call.talha.locker/v1/call', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ calls })
+      }).then(r => r.json() as any);
+    } catch {
+      setLoading(false)
+      return;
+    }
+
+    const took = Date.now() - t1
+
+    const marketAssetName = cvToJSON(hexToCV(resp.calls[0].result)).value.value;
+    const marketAssetSymbol = cvToJSON(hexToCV(resp.calls[1].result)).value.value;
+    const marketAssetDecimals = cvToJSON(hexToCV(resp.calls[2].result)).value.value;
+    const shareTokenName = cvToJSON(hexToCV(resp.calls[3].result)).value.value;
+    const shareTokenSymbol = cvToJSON(hexToCV(resp.calls[4].result)).value.value;
+    const shareTokenDecimals = cvToJSON(hexToCV(resp.calls[5].result)).value.value;
+    const collateralName = cvToJSON(hexToCV(resp.calls[6].result)).value.value;
+    const collateralSymbol = cvToJSON(hexToCV(resp.calls[7].result)).value.value;
+    const collateralDecimals = cvToJSON(hexToCV(resp.calls[8].result)).value.value;
+    const collateralSettings = cvToJSON(hexToCV(resp.calls[9].result)).value.value;
+    const maxLTV = collateralSettings["max-ltv"].value;
+    const liquidationLTV = collateralSettings["liquidation-ltv"].value;
+    const liquidationPremium = collateralSettings["liquidation-premium"].value;
+    const lpParams = cvToJSON(hexToCV(resp.calls[10].result)).value;
+    const totalAssets = lpParams["total-assets"].value;
+    const totalShares = lpParams["total-shares"].value;
+    const debtParams = cvToJSON(hexToCV(resp.calls[11].result)).value;
+    const openInterest = debtParams["open-interest"].value;
+    const totalDebtShares = debtParams["total-debt-shares"].value;
+    const accrueInterestParams = cvToJSON(hexToCV(resp.calls[12].result)).value;
+    const lastInterestAccruedBlockTime = accrueInterestParams.value["last-accrued-block-time"].value;
+    const freeLiquidity = cvToJSON(hexToCV(resp.calls[13].result)).value;
+    const reserveBalance = cvToJSON(hexToCV(resp.calls[14].result)).value;
+    const collateralBalance = cvToJSON(hexToCV(resp.calls[15].result)).value.value;
+    const collateralsDepositedBalance = cvToJSON(hexToCV(resp.calls[16].result)).value.value.amount.value;
+    const userPosition = cvToJSON(hexToCV(resp.calls[17].result)).value.value;
+    const userDebtShares = userPosition["debt-shares"].value;
+    const userLpShares = userPosition["borrowed-amount"].value;
+    const marketAssetWalletBalance = cvToJSON(hexToCV(resp.calls[18].result)).value.value;
+    const collateralsWalletBalance = cvToJSON(hexToCV(resp.calls[19].result)).value.value;
+    const irParams = cvToJSON(hexToCV(resp.calls[20].result)).value;
+    const baseIr = irParams["base-ir"].value;
+    const irSlope1 = irParams["ir-slope-1"].value;
+    const irSlope2 = irParams["ir-slope-2"].value;
+    const utilizationKink = irParams["utilization-kink"].value;
+    const lpCapFactor = cvToJSON(hexToCV(resp.calls[21].result)).value;
+    const debtCapFactor = cvToJSON(hexToCV(resp.calls[22].result)).value;
+    const collateralCapFactor = cvToJSON(hexToCV(resp.calls[23].result)).value;
+    const lpBucket = cvToJSON(hexToCV(resp.calls[24].result)).value;
+    const debtBucket = cvToJSON(hexToCV(resp.calls[25].result)).value;
+    const collateralBucket = cvToJSON(hexToCV(resp.calls[26].result)).value;
+    const lpIncentives = resp.calls.slice(27).map((i: any, x: number) => {
+      const startDate = new Date(incentives[x].startDate * 1000).toLocaleDateString("en-US", { month: 'long', day: 'numeric' });
+      const endDate = new Date(incentives[x].endDate * 1000).toLocaleDateString("en-US", { month: 'long', day: 'numeric' })
+
+      let rewards = null;
+
+      const inner = cvToJSON(hexToCV(i.result)).value.value;
+      if (inner !== null) {
+        rewards = {
+          "claimedRewards": inner.value["claimed-rewards"].value,
+          "earnedRewards": inner.value["earned-rewards"].value,
+        };
+      }
+
+      return {
+        key: incentives[x].startDate,
+        date: `${startDate}-${endDate}`,
+        rewards
+      }
+    })
+    console.log(lpIncentives)
+
+    setResp({
+      marketAsset: {
+        name: marketAssetName,
+        symbol: marketAssetSymbol,
+        decimals: marketAssetDecimals
+      },
+      shareToken: {
+        name: shareTokenName,
+        symbol: shareTokenSymbol,
+        decimals: shareTokenDecimals
+      },
+      collateral: {
+        name: collateralName,
+        symbol: collateralSymbol,
+        decimals: collateralDecimals,
+        maxLTV,
+        liquidationLTV,
+        liquidationPremium,
+      },
+      marketState: {
+        totalAssets,
+        totalShares,
+        openInterest,
+        totalDebtShares,
+        lastInterestAccruedBlockTime,
+        freeLiquidity,
+        reserveBalance,
+        collateralBalance
+      },
+      userState: {
+        user,
+        collateralsDepositedBalance,
+        debtShares: userDebtShares,
+        lpShares: userLpShares,
+        marketAssetWalletBalance,
+        collateralsWalletBalance
+      },
+      irParams: {
+        baseIr,
+        irSlope1,
+        irSlope2,
+        utilizationKink
+      },
+      caps: {
+        lpCapFactor,
+        debtCapFactor,
+        collateralCapFactor,
+        lpBucket,
+        debtBucket,
+        collateralBucket
+      },
+      lpIncentives
+    });
+
+    setTook(took);
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <button onClick={fetchClicked}>{loading ? '...' : 'fetch state'}</button>
+      <br /><br />
+      {took > 0 ?
+        <div>
+          {calls.length} contract calls, took {took}ms <br /><br />
+          <pre>{JSON.stringify(resp, null, 2)}</pre>
+        </div>
+        : null}
+    </>
+  )
+}
+
+export default App
---
+++ src/lp-incentives.ts
@@ -0,0 +1,264 @@
+export const AEUSDC = {
+    id: "aeUSDC",
+    name: "token-aeusdc",
+    principal: "SP3Y2ZSH8P7D50B0VBTSX11S7XSG24M1VB9YFQA4K",
+};
+
+export const SBTC = {
+    id: "sbtc-token",
+    name: "sbtc-token",
+    principal: "SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4",
+};
+
+export const STX = {
+    id: "stx",
+    name: "stx",
+    principal: "",
+};
+
+const incentives = [
+    {
+        startDate: 1744221780,
+        endDate: 1744122780,
+        rewards: 10000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SP37SS3Z22V937APV1V7F5VYDKWWX4925K2B2ZMHC",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1744302900,
+        endDate: 1744821300,
+        rewards: 1260000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SP3ENJM52T5VE8EPK6S3A86NCWWQV8968GXVR9RQX",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1744824900,
+        endDate: 1745418900,
+        rewards: 1230000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SP1MX62KQEB7WPHWA5V8D1VSJ0J80S9CBXJQ7Q18Q",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1745422500,
+        endDate: 1746009300,
+        rewards: 2451000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SP26EDKK2BGBYHJW9KHK8EF9N9R3F3GQRSAFNTNYJ",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1746002100,
+        endDate: 1746635700,
+        rewards: 2685000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SP37JC08D5EM2T53JF09Y0ZEQKYTVW1Y8K7VDDGAC",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1746636900,
+        endDate: 1747240500,
+        rewards: 4802000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SPQMP7TPG5C1P2FJMBJPVM1E0F751CFQWJJQ93F8",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1747241100,
+        endDate: 1747841700,
+        rewards: 9890000000,
+        contract: {
+            id: "lp-incentives-v1",
+            name: "lp-incentives-v1",
+            principal: "SP1W8S33K6JBDEDZA2P01N0ARP1M433PBAC1PF123",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1747842900,
+        endDate: 1748446500,
+        rewards: 11830000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP23MK2Z02H0D6H1SVFHFYGA45AQDW9HK9XZZXNSY",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1748453100,
+        endDate: 1749050700,
+        rewards: 15185000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP1D1RBYJ97VA3XYJ2Y19E06CX2AKGB4QX12ETBAC",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1749051300,
+        endDate: 1749654000,
+        rewards: 22260000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP6XCN5EE7KHWP4V7JP9K9RZYHK1J7S8M7NXWB5K",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1749655500,
+        endDate: 1750260900,
+        rewards: 28393000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP2JZQ7BXSEV4H8KXD6ZT0NW293V5ENJR7Y12RPJ3",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1750264320,
+        endDate: 1750865100,
+        rewards: 35491000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP2PRX1PQDFAKTP12TFMEZJNG7BAEF66ZVBD4YP24",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1750865700,
+        endDate: 1751469900,
+        rewards: 30625000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SPMCMPRRD1WQHKW5F7PRJW2AZV4K9AEH8SJTQMXF",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1751470500,
+        endDate: 1752074700,
+        rewards: 30685000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP184ER80H9V3JRA36W8EJKZ63NRMB1YPC1HFW55",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1752075300,
+        endDate: 1752679500,
+        rewards: 30685000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP332XB0Q9ZZHM1K6AJJ12FE61RNCQGF0HVWW2T80",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1752680100,
+        endDate: 1753284300,
+        rewards: 32685000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP1BXEWB1EKTCPC9G4SYCXY25P8XAHXCV2EV4A38E",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1753284900,
+        endDate: 1753889100,
+        rewards: 33282000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP3141V7G9K4WYCN2CEV4075S3NV5AFR7JVRJQRGW",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1753889700,
+        endDate: 1754493900,
+        rewards: 33753000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP25C79PQ3HTH73GR63NT7A1JS8D0633HT9KNK2JJ",
+        },
+        token: AEUSDC,
+    },
+    {
+        startDate: 1754494500,
+        endDate: 1755098700,
+        rewards: 56315000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP3VTYKR6QSXZ32E961070K998C5V3SNCNK3KH91Z",
+        },
+        token: STX,
+    },
+    {
+        startDate: 1755099300,
+        endDate: 1755703500,
+        rewards: 50402000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP23SGEVCMWBY7M67BEZMG3HHWXGN43KNHJX973PX",
+        },
+        token: STX,
+    },
+    {
+        startDate: 1755704100,
+        endDate: 1756308300,
+        rewards: 58452000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP32Q8R7BS97CYSTNY1BAGF91QHYJPR2S9FBAV8B2",
+        },
+        token: STX,
+    },
+    {
+        startDate: 1756308900,
+        endDate: 1756913100,
+        rewards: 41920000000,
+        contract: {
+            id: "lp-incentives-v2",
+            name: "lp-incentives-v2",
+            principal: "SP2WFSC5DQ7JQXF0AD2CSKDK5PKF8Z51BMPNMZBVR",
+        },
+        token: STX,
+    },
+]
+
+export default incentives;
